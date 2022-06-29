a282701fa59a0443517882ecbec64cee0ded4446
TheyCallMeKWAM <54544795+TheyCallMeKWAM@users.noreply.github.com>
2022-06-29 19:05:53 -0400

    3.6c My PC is Exploding

--- content/layout/custom_game/common/ingame_warning/ingame_warning.js
+++
@@ -1,9 +0,0 @@
-function CloseWarning(panelName) {
-	$("#" + panelName).SetHasClass("hide", true);
-}
-function ScheludeCloseWarning(time, panelName) {
-	$.Schedule(time, () => {
-		CloseWarning(panelName);
-	});
-}
-(function () {})();
